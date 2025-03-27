7ee809ad91029c3fe3154270dbf17445882957a9
eddy <edyasin09@gmail.com>
2025-03-27 21:19:05 +0700

    penerapan awal

---
+++ express-mysql/routes/posts.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+//import database
+var connection = require('../library/database');
+
+/**
+ * INDEX POSTS
+ */
+router.get('/', function (req, res, next) {
+    //query
+    connection.query('SELECT * FROM posts ORDER BY id desc', function (err, rows) {
+        if (err) {
+            req.flash('error', err);
+            res.render('posts', {
+                data: ''
+            });
+        } else {
+            //render ke view posts index
+            res.render('posts/index', {
+                data: rows // <-- data posts
+            });
+        }
+    });
+});
+
+//CREATE POST
+
+
+// EDIT/UPDATE
+
+
+// DELETE
+
+module.exports = router;
